0e6487bbe321ada69224e92fe70bf06bb5162a09
vovan13347 <vova-lebedev-97@mail.ru>
2024-07-26 22:09:39 +0300

    обновление

---
+++ app/static/js/script.js
@@ -0,0 +1,16 @@
+
+/*
+function mouseover(id) {
+  id.style.fontSize = "50px";
+  id.style.color = "lightgreen";
+};
+
+function mouseout(id) {
+  id.style.fontSize = "32px";
+  id.style.color = "white";
+};
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Javascript работает.");
+})
