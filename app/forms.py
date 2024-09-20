03587136011b69e6ea1c7b32c617ff68aae9e41d
vovan13347 <vova-lebedev-97@mail.ru>
2024-09-20 22:35:44 +0300

    update css

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-  username = StringField('Имя пользозателя', validators=[DataRequired()])
+  username = StringField('Логин пользозателя', validators=[DataRequired()])
   password = PasswordField('Пароль', validators=[DataRequired()])
   remember_me = BooleanField('Запомнить')
   submit = SubmitField('Войти')
