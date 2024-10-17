e3013fcfad6a8416df2d03a44ba2237cfa569a69
vovan13347 <vova-lebedev-97@mail.ru>
2024-10-17 23:46:51 +0300

    add data

---
+++ _user__data.sql
@@ -0,0 +1,9 @@
+INSERT INTO "user" (username,email,password_hash) VALUES
+	 ('Bestuser','bestuser@gmail.com','1234'),
+	 ('vova','vova@gmail.com','12345'),
+	 ('mark','mark@gmail.com','12345'),
+	 ('poul','poul@gmail.com','12345'),
+	 ('jack','jack@gmail.com','12345'),
+	 ('anna','anna@gmail.com','12345'),
+	 ('sam','sam@gmail.com','12345'),
+	 ('Newuser','Newuser@gmail.com','1234');
