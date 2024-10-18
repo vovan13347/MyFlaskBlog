e4ebf5395e3ed7c56fc5455373f8c342551044da
vovan13347 <vova-lebedev-97@mail.ru>
2024-10-18 03:27:42 +0300

    log

---
+++ user_update.sql
@@ -0,0 +1,11 @@
+INSERT INTO "user" (username,email,password_hash,avatar) VALUES
+	 ('Bestuser','bestuser@gmail.com','1234',NULL),
+	 ('vova','vova@gmail.com','12345',NULL),
+	 ('mark','mark@gmail.com','12345',NULL),
+	 ('poul','poul@gmail.com','12345',NULL),
+	 ('jack','jack@gmail.com','12345',NULL),
+	 ('anna','anna@gmail.com','12345',NULL),
+	 ('sam','sam@gmail.com','12345',NULL),
+	 ('Newuser','Newuser@gmail.com','1234',NULL),
+	 ('myuser','myuser@mail.ru','scrypt:32768:8:1$SzJi9n5tHfRAonlt$fa1f1cae352f43812d8ac603f1b88b82855695b33732737e424a8193dc16739056ba41f0ed1157f650c6622f6d9a1674460edb8566729c899c7bfad5481a06a2','cat.jpg'),
+	 ('helloworlder','helloworlder@mail.ru','scrypt:32768:8:1$2QYorhwfunjqjdmB$7ff50146fbfcc34fa4a8423b266f7c2ca4ce79a8f6c3cdde6e2362b30806c94861a842e3b750a7d080dd71c8dfb34aa13dc8013c30fc096e25dffaf4281564f8','robot.png');
