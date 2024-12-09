fe8cb447be36ed1815116f188e8bcc342f7533be
vovan13347 <vova-lebedev-97@mail.ru>
2024-12-09 16:47:48 +0000

    update javascript

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,3 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("Javascript работает.");
 })
+
+function confirmClick() {
+    return confirm("Вы действительно хотите удалить пост?");
+}
